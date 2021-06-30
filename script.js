6061a13ef0f74b83f145337b40c7c608d1da37fc
teewo0 <teewodavie@gmail.com>
2021-06-30 09:47:18 -0400

    javascript task complete

---
+++ script.js
@@ -0,0 +1,16 @@
+function decrease(){
+    var a = 1;
+    var textBox = document.getElementById("text");
+    textBox.value--;
+
+}
+function reset(){
+    var textBox = document.getElementById("text");
+    textBox.value = 0;
+}
+function increase(){
+    var textBox = document.getElementById("text");
+    textBox.value++;
+
+}
+
