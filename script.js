748a184599cbf745a1c0be184d74fc0ebc362b08
RikHamale100 <r3ezyr3e@gmail.com>
2021-07-01 11:46:29 +0100

    JS TASK DONE

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function decrease(){
-    var a = 1;
-    var textBox = document.getElementById("text");
+    let a = 1;
+    let textBox = document.getElementById("text");
     textBox.value--;
 
 }
 function reset(){
-    var textBox = document.getElementById("text");
+    let textBox = document.getElementById("text");
     textBox.value = 0;
 }
 function increase(){
-    var textBox = document.getElementById("text");
+    let textBox = document.getElementById("text");
     textBox.value++;
-
 }
-
